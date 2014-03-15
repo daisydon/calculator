c0ad710719a77ada8d6ddf6545d800486eb28d47
Linyu Dong <daisydon@brandeis.edu>
2014-03-15 16:49:04 -0700

    added logging

--- calculate.js
+++ calculate.js
@@ -45,50 +45,15 @@ app.post("/", function(req, res) {
 	var num1 = body.num1;
 	var num2 = body.num2;
 	var result = math.add(num1, num2);
-	res.send(html(head(title("response")) + body(util.format("<br/>The result is %d", result))));
+	console.log("result", result);
+	var header = head(title("response"));
+	console.log("header", header);
+	body = util.format("<br/>The result is %d", result);
+	console.log("body", body);
+	var response = html(head + body);
+	console.log("response", response);
+	res.send(response);
 });
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
-
-/*var ipAddress = "127.0.0.1",
-httpModule = require("http"),
-url = require("url"),
-qs = require("querystring"),
-math = require("./add.js"),
-fs = require("fs"),
-form;
-
-
-fs.readFile("./InputForm.html", function(err, data) {
-	if (err) {
-		throw err;
-	}
-	form = data;
-});
-
-httpModule.createServer(
-				exports.request = function serviceRequest(request, response) {
-
-					if (request.method === "GET") {
-						response.end(form);
-					} else if (request.method === "POST") {
-						// create an instance
-						var requestbody = "";
-						request.on("data", function(data) {
-							requestbody += data;
-						});
-						request.on("end", function() {
-											var formData = qs.parse(requestbody);
-											var num1 = formData.num1;
-											var num2 = formData.num2;
-											var ret = math.add(num1, num2);
-											response.write("<html><head><title> resonse </title></head><body>");
-											response.write("<br /> The result is:" + ret);
-
-											response.end("</body> </html>");
-										});
-
-					}
-
-				}).listen(Number(process.env.PORT || 8080), ipAddress);*/
